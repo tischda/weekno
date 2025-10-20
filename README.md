43952442c38684c5e4f92559c87b3da0ef891750
Daniel TISCHER <dos.7182@gmail.com>
2025-10-20 10:40:24 +0200

    update build system

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
 [![Build Status](https://github.com/tischda/weekno/actions/workflows/build.yml/badge.svg)](https://github.com/tischda/weekno/actions/workflows/build.yml)
 [![Test Status](https://github.com/tischda/weekno/actions/workflows/test.yml/badge.svg)](https://github.com/tischda/weekno/actions/workflows/test.yml)
 [![Go Report Card](https://goreportcard.com/badge/github.com/tischda/weekno)](https://goreportcard.com/report/github.com/tischda/weekno)
+[![Linter Status](https://github.com/tischda/weekno/actions/workflows/linter.yml/badge.svg)](https://github.com/tischda/weekno/actions/workflows/linter.yml)
+[![License](https://img.shields.io/github/license/tischda/weekno)](/LICENSE)
+[![Release](https://img.shields.io/github/release/tischda/weekno.svg)](https://github.com/tischda/weekno/releases/latest)
 
 # weekno
 
 Prints the current week number.
 
-### Install
+## Install
 
 ~~~
 go install github.com/tischda/weekno@latest
 ~~~
 
-### Usage
+## Usage
 
 ~~~
 weekno
--- main.go
+++ main.go
@@ -1,14 +1,67 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
 	"time"
 )
 
-// set via -ldflags
-var version string
+// https://goreleaser.com/cookbooks/using-main.version/
+var (
+	name    string
+	version string
+	date    string
+	commit  string
+)
+
+// flags
+type Config struct {
+	help    bool
+	version bool
+}
+
+func initFlags() *Config {
+	cfg := &Config{}
+	flag.BoolVar(&cfg.help, "?", false, "")
+	flag.BoolVar(&cfg.help, "help", false, "displays this help message")
+	flag.BoolVar(&cfg.version, "v", false, "")
+	flag.BoolVar(&cfg.version, "version", false, "print version and exit")
+	return cfg
+}
 
 func main() {
+	log.SetFlags(0)
+	cfg := initFlags()
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage: "+name+` [OPTIONS]
+
+Displays the week number of the current date.
+
+OPTIONS:
+
+  -?, --help
+        display this help message
+  -v, --version
+        print version and exit`)
+	}
+	flag.Parse()
+
+	if flag.Arg(0) == "version" || cfg.version {
+		fmt.Printf("%s %s, built on %s (commit: %s)\n", name, version, date, commit)
+		return
+	}
+
+	if cfg.help {
+		flag.Usage()
+		return
+	}
+
+	if len(os.Args) > 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
 	_, week := time.Now().ISOWeek()
 	fmt.Printf("%d", week)
 }
